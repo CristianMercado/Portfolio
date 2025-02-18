49d1a36039ae725c6fbe406eafdc661482bede97
CristianMercado <crisalemerto@gmail.com>
2025-02-17 20:52:11 -0600

    Adding Summary Section

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
